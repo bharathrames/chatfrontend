1019f458cd3cefc8c8022a4c19544c515ac03835
bharathrames <bharath91505@gmail.com>
2023-06-29 21:14:39 +0530

    re

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://studentsbackend.vercel.app/";
+export const host = "https://studentsbackend.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
