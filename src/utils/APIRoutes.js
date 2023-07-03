279c39669eb8ba8699c9b8212df40b7b90733fa2
bharathrames <bharath91505@gmail.com>
2023-07-03 12:15:52 +0530

    routes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 export const host = "https://chat-backend-sigma-seven.vercel.app";
-export const loginRoute = `${host}/api/auth/login`;
+export const loginRoute = "https://chat-backend-sigma-seven.vercel.app/api/auth/login";
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const logoutRoute = `${host}/api/auth/logout`;
